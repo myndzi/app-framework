42cdca96b64acdff659bcb86835399727d5e3ee9
Kris Reeves <krisreeves@searchfanatics.com>
2018-01-21 16:29:51 -0800

    Use 'stoppable' fork to more cleanly shut down http server

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -3,21 +3,25 @@
 require('should-eventually');
 
 describe('app', function () {
-    var app;
-
-    before(function () {
+    beforeEach(function () {
+        var self = this;
         return require('../../lib/index')({
             app: {
               env: 'testing',
               root: __dirname
             }
         }).then(function (_app) {
-            app = _app;
-            return app.listen();
+            self.app = _app;
+            return _app.listen();
         });
     });
 
+    afterEach(function () {
+        return this.app.shutdown();
+    });
+
     it('should have methods \'lib\', \'path\', and properties \'log\', \'config\', \'root\'', function () {
+        var app = this.app;
         app.should.have.property('lib').which.is.a.Function;
         app.should.have.property('path').which.is.a.Function;
         app.should.have.property('root').equal(__dirname);
@@ -26,12 +30,14 @@ describe('app', function () {
     });
 
     it('should be using the null-logger when called with quiet: true', function () {
+        var app = this.app;
         var mock = { };
         require('../../lib/null-logger')(mock);
         mock.log.debug.should.equal(app.log.debug);
     });
 
     it('should emit a \'loaded\' event when complete', function (done) {
+        var app = this.app;
         return require('../../lib/index')({
             root: __dirname,
             quiet: true
@@ -40,7 +46,8 @@ describe('app', function () {
         });
     });
 
-    it('should emit a \'shutdown\' method when calling app.shutdown()', function () {
+    it('should emit a \'shutdown\' event when calling app.shutdown()', function () {
+        var app = this.app;
         var called = false;
         app.on('shutdown', function () {
             called = true;
